5598b82d194ff896917e0f1797a1d807f357c922
therealpanda98 <therealpanda@mail.ru>
2020-01-07 02:28:13 +0300

    First three section of portfolio

---
+++ main.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 8000;
+
+/**
+ * Путь для статических файлов
+ */
+app.use(express.static(path.join(__dirname, '/')));
+
+/**
+ * Отрисовка index.html при переходе на 'localhost'
+ */
+app.get('/', (req, res) => {
+	res.sendFile('index.html');
+})
+
+/**
+ * Middleware обработки ошибок
+ */
+app.use((error, req, res, next) => {
+	console.log(error)
+	res.status(500).send('Something broke!')
+})
+
+
+app.listen(port, (error) => {
+	if (error) {
+		return console.log('something bad happened', error)
+	}
+	console.log(`server is listening on ${port}`)
+})
